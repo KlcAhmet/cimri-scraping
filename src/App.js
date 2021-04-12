45df425edbed206580c7d67c3ec78688f78fa5e7
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-12 07:05:25 +0300

    imports App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {
+  Router, Switch, Route
+} from "react-router-dom";
 
+import { Home } from './map/ComponentMap'
+import { history } from './map/UtilsMap'
 
 function App() {
   return (
     <div className="App">
-      test
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
