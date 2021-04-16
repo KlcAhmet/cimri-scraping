90af19f838517387d3940dce1e73df34bf7bf76e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-17 00:45:59 +0300

    Cimri.js firsatlar fonksiyonu yazıldı

--- src/utils/cheerio/cimri.js
+++ src/utils/cheerio/cimri.js
@@ -2,9 +2,33 @@ import cheerio from 'cheerio'
 
 // eslint-disable-next-line
 export default {
-    firsatlarCimri(data) {
-        const $asd = cheerio.load(data);
-        console.log($asd('.jnSyqP', data));
+    async firsatlarCimri(data) {
+        const $products = cheerio.load(data);
+        const productsData = []
+        for (let i = 0; i < $products('.jnSyqP', data).length; i++) {
+            const dataObject = {
+                productImageSrc: null,     // ürün resmi
+                productLink: null,         // ürün linki
+                productTitle: null,        // ürün başlığı
+                productDiscount: null,     // ürün indirim %'desi
+                productLastPrice: null,    // ürün eski fiyatı
+                productNewPrice: null,     // ürün yeni fiyatı
+            }
+            // eslint-disable-next-line
+            if ($products('.jCiUCb > a >img')[i].attribs['data-src'] == 'undefined' || $products('.jCiUCb > a >img')[i].attribs['data-src'] == null) {
+                dataObject.productImageSrc = $products('.jCiUCb > a >img')[i].attribs['src']
+            }
+            else {
+                dataObject.productImageSrc = $products('.jCiUCb > a >img')[i].attribs['data-src']
+            }
+            dataObject.productLink = $products('.btxVWM')[i].attribs['href']
+            dataObject.productTitle = $products('.ibdBUM')[i].attribs['title']
+            dataObject.productDiscount = $products('.kaEwfN')[i].firstChild.data
+            dataObject.productLastPrice = $products('.fFnjMz')[i].firstChild.data
+            dataObject.productNewPrice = $products('.cRSgrJ')[i].firstChild.data
 
+            productsData.push(dataObject)
+        }
+        console.log(productsData);
     }
 }
