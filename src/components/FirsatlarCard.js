f60bf0dc0b2406b28655bce09c98b1bac302fa81
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-18 02:53:38 +0300

    create FirsatlarCard.js

---
+++ src/components/FirsatlarCard.js
@@ -0,0 +1,21 @@
+import { Card } from 'react-bootstrap';
+
+function FirsatlarCard({ props }) {
+
+    return (
+        <Card className="FirsatlarCard" style={{ width: '18rem' }} onClick={() => { window.location.href = props.productLink }}>
+            <Card.Img className="FirsatlarCard-topImg" variant="top" src={props.productImageSrc} />
+            <Card.Body className="FirsatlarCard-body">
+                <Card.Title className="FirsatlarCard-title">{props.productTitle}</Card.Title>
+                <div className="FirsatlarCard-priceBody">
+                    <span className="FirsatlarCard-priceBody-discount">{`%${props.productDiscount}`}</span>
+                    <span className="FirsatlarCard-priceBody-lastPrice">{props.productLastPrice}</span>
+                    <span className="FirsatlarCard-priceBody-newPrice">{props.productNewPrice}</span>
+                </div>
+            </Card.Body>
+            <Card.Img className="FirsatlarCard-bottomImg" variant="center" src={props.productCompanyLogo} />
+        </Card>
+    )
+}
+
+export default FirsatlarCard
