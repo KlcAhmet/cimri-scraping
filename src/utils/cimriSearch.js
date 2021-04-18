23928ce467cb395e4751e63d2a6305700ae465ba
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-19 01:55:56 +0300

    update cimriSearch.js

--- src/utils/cimriSearch.js
+++ src/utils/cimriSearch.js
@@ -1,13 +1,19 @@
+import Post from '../service/Post'
+
+// eslint-disable-next-line
 export default {
     async searchCimri(link) {
         const linkSplit = link.split(' ')
-        if (linkSplit.length === 1) {
-            console.dir(linkSplit);
-        }
-        else {
-
-        }
-
-
+        let postLink = ["arama?q="]
+        linkSplit.forEach(item => {
+            if (item !== "") {
+                // eslint-disable-next-line
+                if (postLink[1] === 'undefined' || postLink[1] == null) {
+                    postLink.push(item)
+                }
+                else postLink.push(`%20${item}`)
+            }
+        })
+        Post.postCimriSearcher(postLink.join(''))
     }
 }
