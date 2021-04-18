f39b82702421b50d9eda7d79555d76e2e1933f4e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-19 02:12:03 +0300

    add Post.js > postCimriSearcher(link)

--- src/service/Post.js
+++ src/service/Post.js
@@ -2,11 +2,14 @@ import axios from 'axios'
 import './axiosInterceptors'
 
 
-const addressCimri = "https://www.cimri.com"
+const addressCimri = "https://www.cimri.com/"
 
 // eslint-disable-next-line
 export default {
     postCimriFirsatlar() {
-        return axios.get(`${addressCimri}/firsatlar`)
+        return axios.get(`${addressCimri}firsatlar`)
+    },
+    postCimriSearcher(link) {
+        return axios.get(`${addressCimri}${link}`)
     }
 }
