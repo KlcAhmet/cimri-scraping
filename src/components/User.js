4aa0669b191fb80e7a2bf83ae1b64d5d74c6800d
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-18 17:33:31 +0300

    create User.js

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import { user } from '../map/ComponentMap'
+
+const User = props => {
+
+    return (
+        <div>
+            <img src={user} />
+            <h6>User Name</h6>
+        </div>
+    )
+}
+
+export default User
