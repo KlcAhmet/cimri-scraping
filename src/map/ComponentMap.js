950e0bdbffe2fe5832efd1f5229ea487cf486d0b
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-18 17:30:06 +0300

    import images > ComponentMap.js

--- src/map/ComponentMap.js
+++ src/map/ComponentMap.js
@@ -3,3 +3,7 @@ export { default as Home } from "../pages/Home"
 /* Components */
 export { default as FirsatlarCard } from "../components/FirsatlarCard"
 export { default as Header } from "../components/Header"
+/* Image */
+export { default as etiket } from "../image/etiket.svg"
+export { default as user } from "../image/user.svg"
+export { default as yuzde } from "../image/yuzde.svg"
