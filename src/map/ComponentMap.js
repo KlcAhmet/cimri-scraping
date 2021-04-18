bee9f56cf62365c6f6a0249c09954b44b9e6684f
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-18 15:58:36 +0300

    import Header.js > ComponentMap.js

--- src/map/ComponentMap.js
+++ src/map/ComponentMap.js
@@ -2,3 +2,4 @@
 export { default as Home } from "../pages/Home"
 /* Components */
 export { default as FirsatlarCard } from "../components/FirsatlarCard"
+export { default as Header } from "../components/Header"
