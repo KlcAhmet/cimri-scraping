cdee3a25c9f2d0d13b502fd0ffbb5fdc7b3b6f35
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-19 01:04:11 +0300

    create cimriSearch.js

---
+++ src/utils/cimriSearch.js
@@ -0,0 +1,13 @@
+export default {
+    async searchCimri(link) {
+        const linkSplit = link.split(' ')
+        if (linkSplit.length === 1) {
+            console.dir(linkSplit);
+        }
+        else {
+
+        }
+
+
+    }
+}
