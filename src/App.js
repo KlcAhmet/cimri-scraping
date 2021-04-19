782c7e01a17b91996ac1f47223fe94b541c8b124
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-19 23:35:42 +0300

    App.js Test Router denendi

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import {
   Router, Switch, Route
 } from "react-router-dom";
 /* Map */
-import { Home, Header } from './map/ComponentMap'
+import { Home, Header, ProductsPage } from './map/ComponentMap'
 import { history } from './map/UtilsMap'
 /* Components */
 import { Container } from 'react-bootstrap';
+import { useSelector } from "react-redux";
+import { useEffect } from 'react';
 /* CSS */
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const Link = useSelector(state => state.searchCimri.link)
+
+  useEffect(() => {
+    if (Link) {
+      history.push(Link)
+    }
+
+  }, [Link])
+
+
   return (
     <div className="App">
       <Router history={history}>
         <Header />
         <Container>
           <Switch>
-            <Route path="/" component={Home} />
+            <Route exact path="/" component={Home} />
+            <Route path="/:Link" component={ProductsPage} />
           </Switch>
         </Container>
       </Router>
