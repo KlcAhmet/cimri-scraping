45df50efd0d15a39f9605f07a4b620e7d25c8a12
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-19 05:27:07 +0300

    add new response condition interceptor

--- src/service/axiosInterceptors.js
+++ src/service/axiosInterceptors.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 import { Cimri } from '../map/UtilsMap'
-import store, { firsatlar } from "../store/index"
+import store, { firsatlar, searchCimri } from "../store/index"
 
 axios.interceptors.response.use(function (response) {
     if (response.config.url === "https://www.cimri.com/firsatlar") store.dispatch(firsatlar(Cimri.firsatlarCimri(response.data)))
-    else if (response.config.url.substring(0, 30) === "https://www.cimri.com/arama?q=") Cimri.searchCimri(response.data)
+    else if (response.config.url.substring(0, 30) === "https://www.cimri.com/arama?q=") store.dispatch(searchCimri(Cimri.searchCimri(response.data)))
 
     return response;
 }, function (err) {
