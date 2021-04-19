b63b1701a0f11e5c9fb3b7047394cc6a31ce6e67
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-20 00:18:12 +0300

    import SeachCard.js > ComponentMap.js

--- src/map/ComponentMap.js
+++ src/map/ComponentMap.js
@@ -5,6 +5,7 @@ export { default as ProductsPage } from "../pages/ProductsPage"
 export { default as FirsatlarCard } from "../components/FirsatlarCard"
 export { default as Header } from "../components/Header"
 export { default as User } from "../components/User"
+export { default as SearchCard } from "../components/SearchCard"
 /* Image */
 export { default as etiket } from "../image/etiket.svg"
 export { default as user } from "../image/user.svg"
