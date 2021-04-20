ddbc699f97221d5e7eff490a0be72f2f8cdf7648
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-21 02:49:29 +0300

    update ProductPage.js

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,32 +1,33 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { Row, Col, ListGroup } from 'react-bootstrap'
-import { SearchCard, SearchSubCategories } from "../map/ComponentMap"
+import { SearchCard, SearchSubCategories, SearchPagesNumber } from "../map/ComponentMap"
 
 function ProductsPage() {
-    const seachCimri = useSelector(state => state.searchCimri)
+    const searchCimri = useSelector(state => state.searchCimri)
     const [cards, setCard] = useState([])
     const [subCategories, setSubCategory] = useState([])
     const [pagesNumber, setPagesNumber] = useState([])
 
     useEffect(() => {
-        if (seachCimri) {
+        if (searchCimri) {
             try {
                 const cardsArr = []
                 const categoryArr = []
-                seachCimri.productsData.forEach((item, index) => {
+                searchCimri.productsData.forEach((item, index) => {
                     cardsArr.push(<SearchCard key={index} props={item} />)
                 })
-                seachCimri.productsCategory.forEach((item, index) => {
+                searchCimri.productsCategory.forEach((item, index) => {
                     categoryArr.push(<SearchSubCategories key={index} props={item} />)
                 })
                 setSubCategory(categoryArr)
                 setCard(cardsArr)
+                setPagesNumber(<SearchPagesNumber key={0} props={searchCimri.productsPageCount} />)
             } catch (error) {
 
             }
         }
-    }, [seachCimri])
+    }, [searchCimri])
 
     return (
         <Row style={{ marginLeft: 7, marginRight: 7 }}>
