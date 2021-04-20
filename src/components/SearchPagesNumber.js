1f965586f7093a0cb4ae6d34083ab8a2eb183754
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-21 02:49:13 +0300

    update SearchPagesNumber.js

--- src/components/SearchPagesNumber.js
+++ src/components/SearchPagesNumber.js
@@ -1,6 +1,40 @@
-function SearchPagesNumber(props) {
-    console.dir(props)
-    return <p>pages num</p>
+import { Button, ButtonToolbar, ButtonGroup } from 'react-bootstrap'
+import Post from '../service/Post'
+
+function SearchPagesNumber({ props }) {
+    const pageArr = []
+    let firstArrow = null
+    let secondArrow = null
+
+    if (props[0].link === null) firstArrow = <Button type="button">{`<`}</Button>
+    else firstArrow = <Button onClick={() => { Post.postCimriSearcher(props[0].link) }}>{`<`}</Button>
+
+    for (let i = 1; i < props.length - 1; i++) {
+        if (props[i].link === null) pageArr.push(<Button key={i}>...</Button>)
+        else pageArr.push(<Button key={i} onClick={() => { Post.postCimriSearcher(props[i].link) }}>{props[i].number}</Button>)
+    }
+
+    if (props[props.length - 1].link === null) secondArrow = <Button type="button">{`>`}</Button>
+    else secondArrow = <Button onClick={() => { Post.postCimriSearcher(props[props.length - 1].link) }}>{`>`}</Button>
+
+
+    return (
+        <ButtonToolbar aria-label="Toolbar with button groups">
+
+            <ButtonGroup className="mr-2" aria-label="First group">
+                {firstArrow}
+            </ButtonGroup>
+
+            <ButtonGroup className="mr-2" aria-label="Second group">
+                {pageArr}
+            </ButtonGroup>
+
+            <ButtonGroup aria-label="Third group">
+                {secondArrow}
+            </ButtonGroup>
+
+        </ButtonToolbar>
+    )
 }
 
 export default SearchPagesNumber
