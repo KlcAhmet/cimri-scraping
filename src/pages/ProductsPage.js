1553b442e8e99839ba0715675e0937105d3a054b
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-20 04:36:01 +0300

    update ProductsPage.js

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,6 +1,35 @@
-function ProductsPage({ page, products }) {
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { Row, Col } from 'react-bootstrap'
+import { SearchCard } from "../map/ComponentMap"
 
-    return <p>products page</p>
+function ProductsPage() {
+    const seachCimri = useSelector(state => state.searchCimri)
+    const [cards, setCard] = useState([])
+
+    useEffect(() => {
+        if (seachCimri) {
+            const cardsArr = []
+            seachCimri.productsData.forEach((item, index) => {
+                cardsArr.push(<SearchCard key={index} props={item} />)
+            })
+            setCard(cardsArr)
+        }
+    }, [seachCimri])
+
+    return (
+        <Row style={{ marginLeft: 7, marginRight: 7 }}>
+            <Col xs={2} style={{ backgroundColor: 'orange' }}>
+                Alt Kategoriler
+            </Col>
+            <Col xs={10} style={{ backgroundColor: 'blue' }}>
+                Cartlar
+                <Row>
+                    {cards}
+                </Row>
+            </Col>
+        </Row>
+    )
 }
 
 export default ProductsPage
