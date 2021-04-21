6c224116bce3739a63a4ec732d51efe2133be559
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-21 04:35:38 +0300

    update SearchSubCategories

--- src/components/SearchSubCategories.js
+++ src/components/SearchSubCategories.js
@@ -1,8 +1,20 @@
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup, Button } from 'react-bootstrap'
+import Post from '../service/Post'
+import store, { searchCimriSubCategoryLink } from '../store/index'
 
 function SearchSubCategories({ props }) {
-
-    return <ListGroup.Item><a href={`https://www.cimri.com${props.categoryLink}`} target="_blank" rel="noreferrer">{props.categoryName} <span>{props.categoryProductsCount}</span></a></ListGroup.Item>
+    console.dir(props)
+    return (
+        <ListGroup.Item>
+            <Button onClick={() => {
+                store.dispatch(searchCimriSubCategoryLink(`https://www.cimri.com${props.categoryLink}`))
+                Post.postCimriSearcherSubCategory(`https://www.cimri.com${props.categoryLink}`)
+            }}>
+                {props.categoryName}
+                <span>{props.categoryProductsCount}</span>
+            </Button>
+        </ListGroup.Item>
+    )
 }
 
 export default SearchSubCategories
