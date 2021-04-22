0cf0049e25000b274eb4a2ff91276e1edc5a6bf8
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-23 00:46:29 +0300

    update UtilsMap.js

--- src/map/UtilsMap.js
+++ src/map/UtilsMap.js
@@ -1,7 +1,9 @@
 export { default as history } from "../utils/history"
 export { default as Cimri } from "../utils/cheerio/Cimri"
-export { default as cimriSearch } from "../utils/cimriSearch"
 export { default as EventBus } from "../utils/Event Bus/EventBus"
+/* Search Functions */
+export { default as cimriSearch } from "../utils/cimriSearch"
+export { default as googleSearch } from "../utils/googleSearch"
 /* Event Bus */
 export { Events } from "../utils/Event Bus/EventBusEvents"
 /* Consts */
