925d14eaec1e8d92caa20d2dd7124aec7104e14b
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-25 00:48:40 +0300

    update interceptor

--- src/service/axiosInterceptors.js
+++ src/service/axiosInterceptors.js
@@ -4,9 +4,12 @@ import store, { firsatlar, searchCimri, headerCimri } from "../store/index"
 
 axios.interceptors.response.use(function (response) {
 
+    /* fırsatlar */
     if (-1 < response.config.url.search("https://www.cimri.com/firsatlar")) store.dispatch(firsatlar(Cimri.firsatlarCimri(response.data)))
+    /* arama */
     else if (-1 < response.config.url.search("https://www.cimri.com/arama?")) store.dispatch(searchCimri(Cimri.searchCimri(response.data, response.config.url)))
     else if (response.config.url === store.getState().searchCimriSubCategoryLink) store.dispatch(searchCimri(Cimri.searchCimri(response.data, response.config.url)))
+    /* header */
     else if (-1 < response.config.url.search("https://www.cimri.com/")) store.dispatch(headerCimri(Cimri.headerCimri(response.data, response.config.url)))
 
     return response;
