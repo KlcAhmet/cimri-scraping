785972a70a9fe00eb8e40e79baeab46472877fa7
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-24 04:41:04 +0300

    Header.js test yazıldı

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Row, Col, Form, Button, Navbar, Nav, NavDropdown, FormControl } from 'react-bootstrap';
 import { User } from '../map/ComponentMap'
 import { cimriSearch, history } from '../map/UtilsMap'
+import { useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
 
 const Header = props => {
+    const headerCimri = useSelector(state => state.headerCimri)
+    const [navigationBar, setNavigation] = useState([])
+    const [navSubCategory, setnavSubCategory] = useState([])
+
+    useEffect(() => {
+        const navArr = []
+
+
+    }, [headerCimri])
+
     return (
         <header>
             <Row>
@@ -26,6 +38,19 @@ const Header = props => {
                     <User />
                 </Col>
             </Row>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 }
