f7d415fb4e2743542783daba01e973602c881d87
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-28 02:43:03 +0300

    backend/usersinfo.js getinfo eklendi

--- backend/routes/usersinfo.js
+++ backend/routes/usersinfo.js
@@ -27,4 +27,58 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
+router.route('/getuserinfo/').get((req, res) => {
+    const userID = req.body.userID
+    const name = req.body.name
+    const surname = req.body.surname
+    const gender = Number(req.body.gender)
+    const birth = Date(req.body.birth)
+    const city = req.body.city
+    const address = req.body.address
+
+    UserInfo.find({
+        "userID": userID
+    })
+        .then((response) => {
+
+            if (response.length) {
+                res.json({
+                    type: 'userinfo',
+                    isSuccess: true,
+                    response: response[0]
+                })
+            }
+            else {
+                res.json(
+                    {
+                        type: 'userinfo',
+                        isSuccess: false,
+                        response: null
+                    }
+                )
+            }
+        })
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+
+
+
+/*    UserInfo.findById(req.params.id)
+        .then((response) => {
+            response.userPassword = userPassword
+ 
+            res.json({
+                type: 'changepassword',
+                isSuccess: true,
+            })
+            response.save()
+        })
+        .catch(err => res.status(400).json('Error: ' + err)) */
+/*  response.surname = surname
+ response.gender = gender
+ response.birth = birth
+ response.city = city
+ response.address = address */
+
 module.exports = router
