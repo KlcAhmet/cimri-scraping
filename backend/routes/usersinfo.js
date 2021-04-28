8c94cf5bda261d03a52be457aff5d8693555f31e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-29 02:30:58 +0300

    update

--- backend/routes/usersinfo.js
+++ backend/routes/usersinfo.js
@@ -27,7 +27,7 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
-router.route('/getuserinfo/').get((req, res) => {
+router.route('/getuserinfo').get((req, res) => {
     const userID = req.body.userID
 
     UserInfo.find({
