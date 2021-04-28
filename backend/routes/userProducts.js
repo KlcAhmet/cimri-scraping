564e7112c9372278980f19ea4085e6fd7bb601ae
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-29 01:50:26 +0300

    backend/userproducts.js "add" route eklendi

--- backend/routes/userProducts.js
+++ backend/routes/userProducts.js
@@ -7,4 +7,22 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
+
+//değişecek
+router.route('/add').post((req, res) => {
+    const userID = Number(req.body.userID)
+    const favorite = req.body.favorite
+    const priceAlarm = req.body.priceAlarm
+
+
+    const newUserProduct = new UserProducts({ userID, favorite, priceAlarm })
+
+    newUserProduct.save()
+        .then(() => res.json({
+            type: 'productadd',
+            isSuccess: true,
+        }))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
 module.exports = router
