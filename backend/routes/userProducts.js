fc7bdd7559f80a50b92496bc048c1d63fd5ffb9e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-29 02:35:16 +0300

    backend/userProducts.js "getproducts" route eklend

--- backend/routes/userProducts.js
+++ backend/routes/userProducts.js
@@ -8,7 +8,6 @@ router.route('/').get((req, res) => {
 })
 
 
-//değişecek
 router.route('/add').post((req, res) => {
     const userID = Number(req.body.userID)
     const favorite = req.body.favorite
@@ -22,6 +21,34 @@ router.route('/add').post((req, res) => {
             type: 'productadd',
             isSuccess: true,
         }))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/getproducts/').get((req, res) => {
+    const userID = req.body.userID
+
+    UserProducts.find({
+        "userID": userID
+    })
+        .then((response) => {
+
+            if (response.length) {
+                res.json({
+                    type: 'productinfo',
+                    isSuccess: true,
+                    response: response[0]
+                })
+            }
+            else {
+                res.json(
+                    {
+                        type: 'productinfo',
+                        isSuccess: false,
+                        response: null
+                    }
+                )
+            }
+        })
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
