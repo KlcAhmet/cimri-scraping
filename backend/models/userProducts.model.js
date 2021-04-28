c698d17bf352e8b9f527c585e10c552acf4403a2
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-28 03:57:06 +0300

    create userProducts.model.js

---
+++ backend/models/userProducts.model.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const favoriteOffers = new Schema({
+    offerLink: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    offerName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    offerPrice: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+})
+
+const favorite = new Schema({
+    productLink: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    productImageSrc: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    productTitle: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    offers: [favoriteOffers]
+})
+
+//---------------------------
+
+const priceAlarm = new Schema({
+    productLink: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    productImageSrc: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    productTitle: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    productPrice: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+})
+
+/* Main Schema */
+
+const userProducts = new Schema({
+    userID: {
+        type: Number,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    favorite: [favorite],
+    priceAlarm: [priceAlarm],
+})
+
+const UserProducts = mongoose.model('UserProducts', userProducts)
+
+module.exports = UserProducts
