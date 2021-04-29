cd817719919702523af3f42613d058caa1697b3e
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-29 23:49:50 +0300

    backend/cimri.js /search router eklendi

--- backend/routes/cimri.js
+++ backend/routes/cimri.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const axios = require('axios');
 const Cimri = require('../utils/cheerio/Cimri')
+const cimriSearch = require('../utils/cimriSearch')
 
 const addressCimri = "https://www.cimri.com/"
 
@@ -23,6 +24,21 @@ router.route('/firsatlar').get((req, res) => {
     axios.get(`${addressCimri}firsatlar`)
         .then(async function (response) {
             const data = await Cimri.firsatlarCimri(response.data)
+            res.json({
+                data: data
+            })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+})
+
+router.route('/search').get((req, res) => {
+    axios.get(`${addressCimri}${req.body.searchLink}`)
+        .then(async function (response) {
+            const data = await Cimri.searchCimri(response.data, response.config.url)
+            console.dir(data)
             res.json({
                 data: data
             })
