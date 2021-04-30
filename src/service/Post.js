97ed45dc26f037adbc7c8d77ff265620b1dc15bc
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 00:11:58 +0300

    update Post.js

--- src/service/Post.js
+++ src/service/Post.js
@@ -1,22 +1,36 @@
 import axios from 'axios'
 import './axiosInterceptors'
 
+const uri = process.env.REACT_APP_SERVER_URI
+const port = process.env.REACT_APP_PORT || 5000
 
-const addressCimri = "https://www.cimri.com/"
-
-// eslint-disable-next-line
-export default {
-    // Cimri
-    postCimriFirsatlar() {
-        return axios.get(`${addressCimri}firsatlar`)
-    },
-    postCimriSearcher(link) {
-        return axios.get(`${addressCimri}${link}`)
-    },
-    postCimriSearcherSubCategory(link) {
-        return axios.get(link)
-    },
-    postCimriHeader() {
-        return axios.get(addressCimri)
-    },
+function postHeader() {
+    return axios.get(`${uri}${port}cimri/`)
 }
+
+function postFirsatlar() {
+    return axios.get(`${uri}${port}cimri/firsatlar`)
+}
+
+function postSearch(search) {
+
+    const data = { searchLink: search }
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    return axios.post(`${uri}${port}cimri/search`, data, config)
+}
+/*     postCimriSearcher(link) {
+    return axios.get(`${addressCimri}${link}`)
+},
+postCimriSearcherSubCategory(link) {
+    return axios.get(link)
+},
+postCimriHeader() {
+    return axios.get(addressCimri)
+}, */
+
+export { postHeader, postFirsatlar, postSearch }
