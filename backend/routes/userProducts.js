0412bcf578c27c0e765d90476c5f5f13553ce83c
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-30 18:06:49 +0300

    userProducts.js error handle yazıldı

--- backend/routes/userProducts.js
+++ backend/routes/userProducts.js
@@ -2,94 +2,161 @@ const router = require('express').Router();
 let UserProducts = require('../models/userProducts.model')
 
 router.route('/').get((req, res) => {
-    UserProducts.find()
-        .then(response => res.json(response))
-        .catch(err => res.status(400).json('Error: ' + err))
+    try {
+        UserProducts.find()
+            .then(response => res.json(response))
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'find',
+            statusCode: 500
+        })
+    }
 })
 
 
 router.route('/add').post((req, res) => {
-    const userID = Number(req.body.userID)
-    const favorite = req.body.favorite
-    const priceAlarm = req.body.priceAlarm
+    try {
+        const userID = Number(req.body.userID)
+        const favorite = req.body.favorite
+        const priceAlarm = req.body.priceAlarm
 
 
-    const newUserProduct = new UserProducts({ userID, favorite, priceAlarm })
+        const newUserProduct = new UserProducts({ userID, favorite, priceAlarm })
 
-    newUserProduct.save()
-        .then(() => res.json({
+        newUserProduct.save()
+            .then(() => res.json({
+                type: 'productadd',
+                isSuccess: true,
+            }))
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                    type: 'productadd',
+                    statusCode: 400
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
             type: 'productadd',
-            isSuccess: true,
-        }))
-        .catch(err => res.status(400).json('Error: ' + err))
+            statusCode: 500
+        })
+    }
 })
 
 router.route('/getproducts/').get((req, res) => {
-    const userID = req.body.userID
+    try {
+        const userID = req.body.userID
 
-    UserProducts.find({
-        "userID": userID
-    })
-        .then((response) => {
-
-            if (response.length) {
-                res.json({
-                    type: 'productinfo',
-                    isSuccess: true,
-                    response: response[0]
-                })
-            }
-            else {
-                res.json(
-                    {
-                        type: 'productinfo',
-                        isSuccess: false,
-                        response: null
-                    }
-                )
-            }
+        UserProducts.find({
+            "userID": userID
         })
-        .catch(err => res.status(400).json('Error: ' + err))
+            .then((response) => {
+
+                if (response.length) {
+                    res.json({
+                        type: 'productinfo',
+                        isSuccess: true,
+                        response: response[0]
+                    })
+                }
+                else {
+                    res.json(
+                        {
+                            type: 'productinfo',
+                            isSuccess: false,
+                            response: null
+                        }
+                    )
+                }
+            })
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                    type: 'productinfo',
+                    statusCode: 400
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'productinfo',
+            statusCode: 500
+        })
+    }
 })
 
 router.route('/changeproduct/:id').post((req, res) => {
-    const favorite = req.body.favorite
-    const priceAlarm = req.body.priceAlarm
+    try {
+        const favorite = req.body.favorite
+        const priceAlarm = req.body.priceAlarm
 
-    UserProducts.findById(req.params.id)
-        .then((response) => {
-            response.favorite = favorite
-            response.priceAlarm = priceAlarm
+        UserProducts.findById(req.params.id)
+            .then((response) => {
+                response.favorite = favorite
+                response.priceAlarm = priceAlarm
 
-            res.json({
-                type: 'changeproducts',
-                isSuccess: true,
+                res.json({
+                    type: 'changeproducts',
+                    isSuccess: true,
+                })
+                response.save()
             })
-            response.save()
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                    type: 'changeproducts',
+                    statusCode: 400
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'productdelete',
+            statusCode: 500
         })
-        .catch(err => res.status(400).json('Error: ' + err))
+    }
 })
 
 router.route('/:id').delete((req, res) => {
-
-    UserProducts.findByIdAndDelete(req.params.id)
-        .then((response) => {
-            if (response === null) {
+    try {
+        UserProducts.findByIdAndDelete(req.params.id)
+            .then((response) => {
+                if (response === null) {
+                    res.json({
+                        type: 'productdelete',
+                        isSuccess: false,
+                        response: response,
+                    })
+                }
+                else {
+                    res.json({
+                        type: 'productdelete',
+                        isSuccess: true,
+                        response: response
+                    })
+                }
+            })
+            .catch(function (error) {
                 res.json({
+                    success: false,
                     type: 'productdelete',
-                    isSuccess: false,
-                    response: response,
+                    statusCode: 400
                 })
-            }
-            else {
-                res.json({
-                    type: 'productdelete',
-                    isSuccess: true,
-                    response: response
-                })
-            }
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'productdelete',
+            statusCode: 500
         })
-        .catch(err => res.status(400).json('Error: ' + err))
+    }
 })
 
 module.exports = router
