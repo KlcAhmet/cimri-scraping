87a9b53c84f22cf734633049cbf091f4b40cc491
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 00:30:49 +0300

    axiosInterceptor.js yeni koşullar yazıldı

--- src/service/axiosInterceptors.js
+++ src/service/axiosInterceptors.js
@@ -1,22 +1,33 @@
 import axios from "axios"
 import store, { firsatlar, searchCimri, headerCimri } from "../store/index"
 
+
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    console.dir(config)
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+
 axios.interceptors.response.use(function (response) {
 
+    if (response.data.success === true && response.data.type === "header") { store.dispatch(headerCimri(response.data.data)) }
+    else if (response.data.success === true && response.data.type === "firsatlar") { store.dispatch(firsatlar(response.data.data)) }
+    else if (response.data.success === true && response.data.type === "search" && response.data.data === false) {
+        // event bus gelecek
+    }
+    else if (response.data.success === true && response.data.type === "search" && response.data !== null) { store.dispatch(searchCimri(response.data.data, response.config.url)) }
+    console.dir(response)
 
 
 
 
 
-
-
-    /* fırsatlar */
-    // if (-1 < response.config.url.search("https://www.cimri.com/firsatlar")) store.dispatch(firsatlar(Cimri.firsatlarCimri(response.data)))
     /* arama */
-    // else if (-1 < response.config.url.search("https://www.cimri.com/arama?")) store.dispatch(searchCimri(Cimri.searchCimri(response.data, response.config.url)))
     //  else if (response.config.url === store.getState().searchCimriSubCategoryLink) store.dispatch(searchCimri(Cimri.searchCimri(response.data, response.config.url)))
-    /* header */
-    // else if (-1 < response.config.url.search("https://www.cimri.com/")) store.dispatch(headerCimri(Cimri.headerCimri(response.data, response.config.url)))
 
     return response;
 }, function (err) {
