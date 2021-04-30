06d4d805faa24b95e8a6e1e35258ba8ab3aa4c39
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-30 17:59:05 +0300

    backend/user.js error handle yazıldı

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,92 +2,157 @@ const router = require('express').Router();
 let User = require('../models/user.model')
 
 router.route('/').get((req, res) => {
-    User.find()
-        .then(response => res.json(response))
-        .catch(err => res.status(400).json('Error: ' + err))
+    try {
+        User.find()
+            .then(response => res.json(response))
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'find',
+            statusCode: 500
+        })
+    }
 })
 
 router.route('/signup').post((req, res) => {
-    const userMail = req.body.userMail
-    const userPassword = req.body.userPassword
+    try {
+        const userMail = req.body.userMail
+        const userPassword = req.body.userPassword
 
-    const newUser = new User({ userMail, userPassword })
+        const newUser = new User({ userMail, userPassword })
 
-    newUser.save()
-        .then(() => res.json({
+        newUser.save()
+            .then(() => res.json({
+                type: 'register',
+                isSuccess: true,
+            }))
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                    type: 'register',
+                    statusCode: 400
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
             type: 'register',
-            isSuccess: true,
-
-        }))
-        .catch(err => res.status(400).json('Error: ' + err))
+            statusCode: 500
+        })
+    }
 })
 
 router.route('/signin').get((req, res) => {
-    const userMail = req.body.userMail
-    const userPassword = req.body.userPassword
+    try {
+        const userMail = req.body.userMail
+        const userPassword = req.body.userPassword
 
-    User.find({
-        "userMail": userMail,
-        "userPassword": userPassword
-    })
-        .then((response) => {
+        User.find({
+            "userMail": userMail,
+            "userPassword": userPassword
+        })
+            .then((response) => {
 
-            if (response.length) {
-                res.json({
-                    type: 'login',
-                    isSuccess: true,
-                    response: response[0]
-                })
-            }
-            else {
-                res.json(
-                    {
+                if (response.length) {
+                    res.json({
+                        type: 'login',
+                        isSuccess: true,
+                        response: response[0]
+                    })
+                }
+                else {
+                    res.json({
                         type: 'login',
                         isSuccess: false,
                         response: null
                     }
-                )
-            }
+                    )
+                }
+            })
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                    type: 'login',
+                    statusCode: 400
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'login',
+            statusCode: 500
         })
-        .catch(err => res.status(400).json('Error: ' + err))
+    }
 })
 
 router.route('/changepassword/:id').post((req, res) => {
-    const userPassword = req.body.userPassword
+    try {
+        const userPassword = req.body.userPassword
 
-    User.findById(req.params.id)
-        .then((response) => {
-            response.userPassword = userPassword
-
-            res.json({
-                type: 'changepassword',
-                isSuccess: true,
+        User.findById(req.params.id)
+            .then((response) => {
+                response.userPassword = userPassword
+                res.json({
+                    type: 'changepassword',
+                    isSuccess: true,
+                })
+                response.save()
             })
-            response.save()
+            .catch(function (error) {
+                res.json({
+                    success: false,
+                    type: 'changepassword',
+                    statusCode: 400
+                })
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'changepassword',
+            statusCode: 500
         })
-        .catch(err => res.status(400).json('Error: ' + err))
+    }
 })
 
 router.route('/:id').delete((req, res) => {
 
-    User.findByIdAndDelete(req.params.id)
-        .then((response) => {
-            if (response === null) {
+    try {
+        User.findByIdAndDelete(req.params.id)
+            .then((response) => {
+                if (response === null) {
+                    res.json({
+                        type: 'userdelete',
+                        isSuccess: false,
+                        response: "Kullanıcı bulunamadı"
+                    })
+                }
+                else {
+                    res.json({
+                        type: 'userdelete',
+                        isSuccess: true,
+                        response: response
+                    })
+                }
+            })
+            .catch(function (error) {
                 res.json({
+                    success: false,
                     type: 'userdelete',
-                    isSuccess: false,
-                    response: response
+                    statusCode: 400
                 })
-            }
-            else {
-                res.json({
-                    type: 'userdelete',
-                    isSuccess: true,
-                    response: response
-                })
-            }
+            })
+    } catch (error) {
+        res.json({
+            success: false,
+            type: 'userdelete',
+            statusCode: 500
         })
-        .catch(err => res.status(400).json('Error: ' + err))
+    }
 })
 
 module.exports = router
