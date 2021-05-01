abc9a99fc9e3955cb7437cf8b0a4e9e7b46819e0
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 18:41:02 +0300

    create user.model.js

---
+++ src/models/user.model.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(id, userMail, userPassword) {
+        this.id = String(id)
+        this.userMail = String(userMail)
+        this.userPassword = String(userPassword)
+    }
+}
+
+export default User
