021d1b1ea2a07ef43ae8a7033255d5d244867cd5
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 15:18:34 +0300

    Message.js listener eklendi

--- src/utils/Message/Message.js
+++ src/utils/Message/Message.js
@@ -46,6 +46,27 @@ function Message() {
             "hideMethod": "fadeOut"
         })
     })
+
+    EventBus.addListener(Const.events.registerSuccess.type, () => {
+        toastr.clear()
+        toastr.success(Const.events.registerSuccess.message, 'Başarılı', {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-center",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "3000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        })
+    })
 }
 
 export default Message
