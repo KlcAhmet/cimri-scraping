f0373a226d55bb0b0f81df186040196c6e386f78
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 14:15:21 +0300

    Login register bölümü yazıldı

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Tabs, Tab, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
-import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
+import { Button, FormGroup, InputGroup } from "@blueprintjs/core"
+import { equalpasswords } from '../map/UtilsMap'
+import { postLogin } from '../map/ServiceMap'
 
 
 const Login = props => {
@@ -10,21 +12,52 @@ const Login = props => {
     return (
         <Container>
             <Row>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                }}>
-                    <FormGroup className="login-form-group" label="Email" labelFor="text-email" labelInfo="(Zorunlu)">
-                        <InputGroup type="email" id="text-email" placeholder="deneme@deneme.com" defaultValue="semihcetin34@gmail.com" />
-                    </FormGroup>
-                    <FormGroup className="login-form-group" label="Şifre" labelFor="text-password" labelInfo="(Zorunlu)">
-                        <InputGroup type="password" id="text-password" placeholder="*******" defaultValue="1" />
-                    </FormGroup>
-                    <div className="login-form-btn">
-                        <Button className="login-form-btn-giris buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="submit">Giriş</Button>
-                        <Link to="/register"><Button className="login-form-btn-kaydol buttons-btn bp3-button bp3-intent-primary bp3-large bp3-fill" type="button">Kaydol</Button></Link>
-                    </div>
-                </form>
-                <Link className="login-forgotpassword" to="/forgotpassword">Şifremi unuttum</Link>
+                <Col>
+                    <Tabs defaultActiveKey="uyeol" id="uncontrolled-tab-example">
+                        <Tab eventKey="giris" title="Giriş Yap">
+                            <form onSubmit={(e) => {
+                                e.preventDefault();
+                            }}>
+                                <FormGroup className="login-form-group" label="Email" labelFor="text-email" labelInfo="(Zorunlu)">
+                                    <InputGroup type="email" id="text-email" placeholder="deneme@deneme.com" defaultValue="deneme@deneme.com" required />
+                                </FormGroup>
+                                <FormGroup className="login-form-group" label="Şifre" labelFor="text-password" labelInfo="(Zorunlu)">
+                                    <InputGroup type="password" id="text-password" placeholder="*******" defaultValue="123456" required />
+                                </FormGroup>
+                                <div className="login-form-btn">
+                                    <Button className="login-form-btn-giris buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="submit">Giriş</Button>
+                                </div>
+                            </form>
+                            <Link className="login-forgotpassword" to="/forgotpassword">Şifremi unuttum</Link>
+                        </Tab>
+                        <Tab eventKey="uyeol" title="Üye ol">
+                            <form onSubmit={(e) => {
+                                e.preventDefault();
+                                if (equalpasswords(e.target[1].value, e.target[2].value)) {
+                                    postLogin(e.target[0].value, e.target[1].value)
+                                }
+                                else {
+                                    // 2 pasaport eşleşmiyor
+                                }
+
+                            }}>
+                                <FormGroup className="login-form-group" label="Email" labelFor="text-email-register" labelInfo="(Zorunlu)">
+                                    <InputGroup type="email" id="text-email-register" placeholder="deneme@deneme.com" defaultValue="deneme@deneme.com" required />
+                                </FormGroup>
+                                <FormGroup className="login-form-group" label="Şifre" labelFor="text-password-register" labelInfo="(Zorunlu)">
+                                    <InputGroup type="password" id="text-password-register" placeholder="*******" defaultValue="123456" required />
+                                </FormGroup>
+                                <FormGroup className="login-form-group" label="Şifre Tekrar" labelFor="text-password-register2" labelInfo="(Zorunlu)">
+                                    <InputGroup type="password" id="text-password-register2" placeholder="*******" defaultValue="123456" required />
+                                </FormGroup>
+                                <div className="login-form-btn">
+                                    <Button className="login-form-btn-kaydol buttons-btn bp3-button bp3-intent-primary bp3-large bp3-fill" type="submit">Kaydol</Button>
+                                </div>
+                            </form>
+                        </Tab>
+                    </Tabs>
+                </Col>
+
             </Row>
         </Container>
     )
