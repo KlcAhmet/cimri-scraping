4c209cd0650358330a481873c9b02b77738deee4
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 14:47:19 +0300

    User.js test içerik yazıldı

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,24 @@
 import { user } from '../map/ComponentMap'
+import { Dropdown } from 'react-bootstrap';
 
 const User = props => {
 
     return (
         <div>
-            <img src={user} alt="" />
-            <h6>User Name</h6>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    <img src={user} alt="" />
+                    <h6>User Name</h6>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="uyelik/hesabım">Hesabım</Dropdown.Item>
+                    <Dropdown.Item href="uyelik/favorilerim">Favorilerim</Dropdown.Item>
+                    <Dropdown.Item href="uyelik/fiyatalarm">Fiyat Alarmı</Dropdown.Item>
+                    <Dropdown.Item href="/cikis">Çıkış</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
         </div>
     )
 }
