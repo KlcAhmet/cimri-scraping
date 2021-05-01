8d874543d8af8cbe54fcdd21a56afdb8fc97853b
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 03:13:07 +0300

    Login.js export edildi

--- src/map/ComponentMap.js
+++ src/map/ComponentMap.js
@@ -2,6 +2,7 @@
 export { default as Home } from "../pages/Home"
 export { default as ProductsPage } from "../pages/ProductsPage"
 export { default as ProductNotFound } from "../pages/ProductNotFound"
+export { default as Login } from "../pages/Login"
 /* Components */
 export { default as FirsatlarCard } from "../components/FirsatlarCard"
 export { default as Header } from "../components/Header"
