1c0f417b9215c8186b698c2408c4d70d1040e990
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 14:45:28 +0300

    EventBusEvents.js event eklendi

--- src/utils/Event Bus/EventBusEvents.js
+++ src/utils/Event Bus/EventBusEvents.js
@@ -3,6 +3,6 @@ import { EventBus, Const } from '../../map/UtilsMap'
 
 export function Events(event) {
     /* Search Not Found */
-    if (event === Const.events.SearchNotFound.type) EventBus.emit(Const.events.SearchNotFound.type, Const.events.SearchNotFound.message);
+    if (event === Const.events.wrongpassword.type) EventBus.emit(Const.events.wrongpassword.type);
 
 }
