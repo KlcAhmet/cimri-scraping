db151ea2649f5d5f82a739ca7164cc4f414384b8
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-01 18:51:15 +0300

    EventBusEvents.js loginSuccess eklendi

--- src/utils/Event Bus/EventBusEvents.js
+++ src/utils/Event Bus/EventBusEvents.js
@@ -6,4 +6,5 @@ export function Events(event) {
     if (event === Const.events.wrongpassword.type) EventBus.emit(Const.events.wrongpassword.type);
     else if (event === Const.events.allreadymail.type) EventBus.emit(Const.events.allreadymail.type);
     else if (event === Const.events.registerSuccess.type) EventBus.emit(Const.events.registerSuccess.type);
+    else if (event === Const.events.loginSuccess.type) EventBus.emit(Const.events.loginSuccess.type);
 }
