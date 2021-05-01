a042acfbabd08fd9d736754b452142938a0423f6
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-02 02:52:32 +0300

    update

--- src/models/userProducts.model.js
+++ src/models/userProducts.model.js
@@ -1,81 +1,17 @@
-import { moongose } from 'mongoose'
-const Schema = mongoose.Schema
-
-const favoriteOffers = new Schema({
-    offerLink: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    offerName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    offerPrice: {
-        type: String,
-        required: true,
-        trim: true,
+class priceAlarm {
+    constructor(productLink, productImageSrc, productTitle, productPrice) {
+        this.productLink = String(productLink)
+        this.productImageSrc = String(productImageSrc)
+        this.productTitle = String(productTitle)
+        this.productPrice = String(productPrice)
     }
-})
+}
 
-const favorite = new Schema({
-    productLink: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    productImageSrc: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    productTitle: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    offers: [favoriteOffers]
-})
-
-//---------------------------
-
-const priceAlarm = new Schema({
-    productLink: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    productImageSrc: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    productTitle: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    productPrice: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-})
-
-/* Main Schema */
-
-const userProducts = new Schema({
-    userID: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    favorite: [favorite],
-    priceAlarm: [priceAlarm],
-})
-
-const UserProducts = mongoose.model('UserProducts', userProducts)
+class UserProducts {
+    constructor(userID, [priceAlarm]) {
+        this.userID = String(userID)
+        this.priceAlarm = new priceAlarm(priceAlarm)
+    }
+}
 
 export default UserProducts
