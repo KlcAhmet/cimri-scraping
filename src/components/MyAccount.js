fcad2db29967c1d5ef380053b61eed36f94fe2b1
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-03 01:31:09 +0300

    MyAccount.js input'lar güncelleştirildi

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,43 +1,69 @@
 import { Button, FormGroup, InputGroup } from "@blueprintjs/core"
 import { Row, Col, Form } from 'react-bootstrap'
+import { UserInfoModel } from '../map/ModelMap'
+import { postUserInfoUpdate } from '../map/ServiceMap'
+import { useSelector } from "react-redux"
+import store, { UserInfo } from "../store/index"
+import { useEffect, useState } from "react"
 
 const MyAccount = () => {
+    const userInfo = useSelector(state => state.UserInfo)
+    const userMail = useSelector(state => state.User.userMail)
+    const [leftForm, setLeftForm] = useState(userInfo)
+
+    useEffect(() => {
+        try {
+            // eslint-disable-next-line
+            if (leftForm.name == 'null' || leftForm.name == null) {
+
+            }
+        } catch (error) {
+
+        }
+    }, [leftForm])
 
     return (
         <Row>
             <Col xs={6}>
                 <Form onSubmit={(e) => {
                     e.preventDefault();
-                    console.dir(e.target[0].value);
-                    console.dir(e.target[1].value);
+                    let gender = ""
                     for (let i = 4; i < 7; i++) {
                         if (e.target[i].checked) {
-                            console.dir(e.target[i].id);
+                            gender = e.target[i].id
                         }
                     }
-                    console.dir(e.target[7].value);
-                    console.dir(e.target[8].value);
-                    console.dir(e.target[9].value);
+                    const model = UserInfoModel.UserInfoUpdate = {
+                        name: e.target[0].value,
+                        surname: e.target[1].value,
+                        gender: gender,
+                        birth: e.target[7].value,
+                        city: e.target[8].value,
+                        address: e.target[9].value
+                    }
+                    postUserInfoUpdate(model)
+                    model['id'] = userInfo.id
+                    model['userID'] = userInfo.userID
+                    store.dispatch(UserInfo(model))
                 }}>
                     <FormGroup className="login-form-group" label="Adınız:" labelFor="text-name">
-                        <InputGroup type="text" id="text-name" defaultValue="Ahmet Batuhan" required />
+                        <InputGroup type="text" id="text-name" defaultValue={((leftForm.name === 'null') ? "" : leftForm.name)} required />
                     </FormGroup>
                     <FormGroup className="login-form-group" label="Soyadınız:" labelFor="text-surname">
-                        <InputGroup type="text" id="text-surname" defaultValue="Kılıç" required />
+                        <InputGroup type="text" id="text-surname" defaultValue={((leftForm.name === 'null') ? "" : leftForm.surname)} required />
                     </FormGroup>
                     <FormGroup className="login-form-group" label="E-posta:" labelFor="text-mail">
-                        <InputGroup type="mail" id="text-mail" defaultValue="ahmetbatukilic@gmail.com" disabled />
+                        <InputGroup type="mail" id="text-mail" defaultValue={userMail} disabled />
                     </FormGroup>
                     <fieldset>
                         <FormGroup className="login-form-group" label="Cinsiyet:" labelFor="text-gender">
                             <Form.Check inline label="Erkek" type='radio' id='Erkek' name="genderRadios" />
                             <Form.Check inline label="Kadın" type='radio' id='Kadın' name="genderRadios" />
-                            <Form.Check inline defaultChecked label="Belirtmek istemiyorum" type='radio' id='None' name="genderRadios" />
+                            <Form.Check inline label="Belirtmek istemiyorum" type='radio' id='None' name="genderRadios" />
                         </FormGroup>
-
                     </fieldset>
                     <FormGroup className="login-form-group" label="Doğum Tarihi:" labelFor="date-birth">
-                        <Form.Control type="date" name='date_of_birth' id='date-birth' />
+                        <Form.Control type="date" name='date_of_birth' id='date-birth' required />
                     </FormGroup>
                     <Form.Group controlId="citySelect">
                         <Form.Label>İl:</Form.Label>
@@ -51,10 +77,10 @@ const MyAccount = () => {
                     </Form.Group>
                     <Form.Group controlId="addressArea">
                         <Form.Label>Adres:</Form.Label>
-                        <Form.Control as="textarea" rows={2} />
+                        <Form.Control as="textarea" rows={2} defaultValue={((leftForm.name === 'null') ? "" : leftForm.address)} required />
                     </Form.Group>
                     <div className="login-form-btn">
-                        <Button className="login-form-btn-giris buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="submit">Giriş</Button>
+                        <Button className="login-form-btn-giris buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="submit">Kayıt et</Button>
                     </div>
                 </Form>
             </Col>
