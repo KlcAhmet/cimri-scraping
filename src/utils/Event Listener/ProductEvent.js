34a6edca07a3101cd171d130274b7d373c412aed
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-04 02:37:35 +0300

    ProductEvent.js product silme yazıldı

--- src/utils/Event Listener/ProductEvent.js
+++ src/utils/Event Listener/ProductEvent.js
@@ -1,10 +1,10 @@
 import { Const, EventBus } from '../../map/UtilsMap'
-import store, { productFavoriteUpdate } from "../../store/index"
+import store, { productFavoriteAdd, productFavoriteRemove } from "../../store/index"
 
 function ProductEvent() {
     EventBus.addListener(Const.events.productLike.type, ({ data }) => {
         if (store.getState().User.id) {
-            store.dispatch(productFavoriteUpdate(data))
+            store.dispatch(productFavoriteAdd(data))
         }
         else {
             // event gelecek değişecek
@@ -14,7 +14,16 @@ function ProductEvent() {
 
     EventBus.addListener(Const.events.productUnlike.type, ({ data }) => {
         if (store.getState().User.id) {
-
+            const productArr = []
+            for (let i = 0; i < store.getState().UserProducts.favorite.length; i++) {
+                if (data.productTitle === store.getState().UserProducts.favorite[i].productTitle) {
+                    console.log("bulundu");
+                }
+                else {
+                    productArr.push(store.getState().UserProducts.favorite[i])
+                }
+            }
+            store.dispatch(productFavoriteRemove(productArr))
         }
         else {
             // event gelecek değişecek
