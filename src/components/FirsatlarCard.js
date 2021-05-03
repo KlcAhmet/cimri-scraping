5ccb186c5e7ba0be928af36ce524753f7862dc40
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-04 01:50:45 +0300

    update

--- src/components/FirsatlarCard.js
+++ src/components/FirsatlarCard.js
@@ -1,25 +1,9 @@
-import { useState } from 'react';
-import { Card, Button } from 'react-bootstrap';
-import { likered, likeblack } from '../map/ComponentMap'
-import { Events, Const } from "../map/UtilsMap"
+import { Card } from 'react-bootstrap';
 
 function FirsatlarCard({ props }) {
-    const [like, setlike] = useState({
-        id: "black",
-        img: likeblack
-    })
 
     return (
         <Card className="FirsatlarCard" style={{ width: '18rem' }} >
-            <div>
-                <Button variant="outline-primary" type="button" onClick={(e) => {
-                    setlike(((like.id === 'black') ? { id: 'red', img: likered } : { id: 'black', img: likeblack }))
-                    // event eklenecek
-                    if (like.id === 'red') Events(Const.events.productLike.type, props)
-                    else Events(Const.events.productUnlike.type, props)
-
-                }}><img style={{ width: 20, zIndex: 2 }} src={like.img} alt="like" /></Button>
-            </div>
             <Card.Img className="FirsatlarCard-topImg" variant="top" src={props.productImageSrc} />
             <Card.Body className="FirsatlarCard-body">
                 <Card.Title className="FirsatlarCard-title">{props.productTitle}</Card.Title>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = props => {
             })
         }
         else {
-            postFirsatlar()
+            //  postFirsatlar()
         }
     }, [CimriFirsatlar])
 
