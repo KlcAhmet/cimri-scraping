312fcd69c7f80b0b1593f90f161ce08ff19d2427
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-04 20:54:57 +0300

    User.js dom hatası düzeltildi

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,7 @@
 import { Dropdown } from 'react-bootstrap'
-import { Button, Icon, IconSize, Intent } from "@blueprintjs/core"
+import { Icon, IconSize, Intent } from "@blueprintjs/core"
 import { IconNames } from "@blueprintjs/icons";
 import { history } from '../map/UtilsMap'
-import { Link } from 'react-router-dom';
 
 const User = props => {
 
@@ -13,14 +12,13 @@ const User = props => {
                     <h6><Icon icon={IconNames.PERSON} iconSize={IconSize.LARGE} intent={Intent.PRIMARY} />ttest</h6>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item >
-                        <Button onClick={() => history.push('/login')} className="login-form-btn-giris buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="button">Giriş</Button>
+                    <Dropdown.Item onClick={() => history.push('/login')} >Giriş
                     </Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item><Link className="login-forgotpassword" to="/uyelik/hesabim#hesap">Hesabım</Link></Dropdown.Item>
-                    <Dropdown.Item><Link className="login-forgotpassword" to="/uyelik/hesabim#favori">Favorilerim</Link></Dropdown.Item>
-                    <Dropdown.Item><Link className="login-forgotpassword" to="/uyelik/hesabim#fiyatalarm">Fiyat Alarmı</Link></Dropdown.Item>
-                    <Dropdown.Item><Link className="login-forgotpassword" to="/cikis">Çıkış</Link></Dropdown.Item>
+                    <Dropdown.Item onClick={() => history.push('/uyelik/hesabim#hesap')}>Hesabım</Dropdown.Item>
+                    <Dropdown.Item onClick={() => history.push('/uyelik/hesabim#favori')}>Favorilerim</Dropdown.Item>
+                    <Dropdown.Item onClick={() => history.push('/uyelik/hesabim#fiyatalarm')}>Fiyat Alarmı</Dropdown.Item>
+                    <Dropdown.Item onClick={() => history.push('/cikis')}>Çıkış</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
 
