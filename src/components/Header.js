16e6c574cdef97849e08b126ca9caba8186fbc85
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-06 23:37:48 +0300

    Header.js input için enter event eklendi

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,7 +50,14 @@ const Header = props => {
                         }
                     }}>
                         <Row>
-                            <Col xs={9}><Form.Group controlId="formText"><Form.Control type="text" placeholder="Ürün arayın" /></Form.Group></Col>
+                            <Col xs={9}><Form.Group controlId="formText"><Form.Control type="text" placeholder="Ürün arayın" onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    e.preventDefault()
+                                    if (e.target.value !== "") {
+                                        postSearch(e.target.value)
+                                    }
+                                }
+                            }} /></Form.Group></Col>
                             <Col xs={3}><Button variant="primary" type="submit">ARA</Button></Col>
                         </Row>
                     </Form>
