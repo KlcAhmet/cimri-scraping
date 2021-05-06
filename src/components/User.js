9a0f57774e12cec66d9fb108b81d9f0f4d602300
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-07 02:03:35 +0300

    User.js login butonu display opsiyonlandı

--- src/components/User.js
+++ src/components/User.js
@@ -1,27 +1,41 @@
 import { Dropdown } from 'react-bootstrap'
 import { Icon, IconSize, Intent } from "@blueprintjs/core"
 import { IconNames } from "@blueprintjs/icons";
-import { history } from '../map/UtilsMap'
+import { history, Events, Const } from '../map/UtilsMap'
+import { useSelector } from "react-redux"
+import { useEffect, useState } from 'react';
 
 const User = props => {
+    const userName = useSelector(state => state.UserInfo.name)
+    const userID = useSelector(state => state.User.id)
+    const [id, setId] = useState('Hesabım')
+    const [loginButtonDisplay, setDisplay] = useState('block')
+
+    useEffect(() => {
+        if (userName && userName !== 'null') {
+            setId(userName)
+        }
+        if (userID) {
+            setDisplay('none')
+        }
+
+    }, [userName])
 
     return (
         <div>
             <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    <h6><Icon icon={IconNames.PERSON} iconSize={IconSize.LARGE} intent={Intent.PRIMARY} />ttest</h6>
+                    <span><Icon icon={IconNames.PERSON} iconSize={IconSize.LARGE} intent={Intent.PRIMARY} /> {id}</span>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => history.push('/login')} >Giriş
-                    </Dropdown.Item>
-                    <Dropdown.Divider />
+                    <Dropdown.Item style={{ display: loginButtonDisplay }} onClick={() => history.push('/login')} >Giriş</Dropdown.Item>
+                    <Dropdown.Divider style={{ display: loginButtonDisplay }} />
                     <Dropdown.Item onClick={() => history.push('/uyelik/hesabim#hesap')}>Hesabım</Dropdown.Item>
                     <Dropdown.Item onClick={() => history.push('/uyelik/hesabim#favori')}>Favorilerim</Dropdown.Item>
                     <Dropdown.Item onClick={() => history.push('/uyelik/hesabim#fiyatalarm')}>Fiyat Alarmı</Dropdown.Item>
-                    <Dropdown.Item onClick={() => history.push('/cikis')}>Çıkış</Dropdown.Item>
+                    <Dropdown.Item onClick={() => { Events(Const.events.exitAccount.type) }}>Çıkış</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-
         </div>
     )
 }
