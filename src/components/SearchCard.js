3a267f74473b8548bc3aa385f8c09fddb962e439
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-09 02:15:33 +0300

    SearchCard.js className düzenlendi

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -1,4 +1,4 @@
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Image } from 'react-bootstrap'
 import { useState } from 'react';
 import { likered, likeblack, alarmlike, alarmunlike } from '../map/ComponentMap'
 import { Events, Const } from "../map/UtilsMap"
@@ -16,42 +16,36 @@ function SearchCard({ props, isLike, isAlarm }) {
 
     props.productTopOffers.forEach((item, index) => {
         /* Component */
-        const offer = <a key={index} href={item.offerLink} className="SeachCard-priceBody-link">
-            <p className="SeachCard-priceBody-offerName">{item.offerName}</p>
-            <p className="SeachCard-priceBody-offerPrice">{item.offerPrice}</p>
+        const offer = <a key={index} href={item.offerLink} className="searchCard-body-priceBody-link">
+            <span className="searchCard-body-priceBody-link-offerName">{item.offerName}</span>
+            <span className="searchCard-body-priceBody-link-offerPrice">{item.offerPrice}</span>
         </a>
         /*  */
         offerArr.push(offer)
     })
 
     return (
-        <Card className="SeachCard" style={{ width: '18rem' }} /* onClick={() => { window.location.href = props.productLink }} */>
-            <div>
-                <Button variant="outline-primary" type="button" onClick={(e) => {
+        <Card className="searchCard" /* onClick={() => { window.location.href = props.productLink }} */>
+            <div className="searchCard-buttons">
+                <Button className="searchCard-buttons-likeBtn" variant="outline-primary" type="button" onClick={(e) => {
                     setlike(((like.id === 'black') ? { id: 'red', img: likered } : { id: 'black', img: likeblack }))
-                    if (like.id === 'black') {
-                        Events(Const.events.productLike.type, props)
-                    }
-                    else {
-                        Events(Const.events.productUnlike.type, props)
-                    }
-                }}><img style={{ width: 20, zIndex: 2 }} src={like.img} alt="like" /></Button>
-                <Button variant="outline-primary" type="button" onClick={(e) => {
+                    if (like.id === 'black') Events(Const.events.productLike.type, props)
+                    else Events(Const.events.productUnlike.type, props)
+                }}>
+                    <Image src={like.img} fluid />
+                </Button>
+                <Button className="searchCard-buttons-alarmBtn" variant="outline-primary" type="button" onClick={(e) => {
                     setalarm(((alarm.id === 'alarmunlike') ? { id: 'alarmlike', img: alarmlike } : { id: 'alarmunlike', img: alarmunlike }))
-                    if (alarm.id === 'alarmunlike') {
-                        Events(Const.events.productAlarmLike.type, props)
-                    }
-                    else {
-                        Events(Const.events.productAlarmUnlike.type, props)
-                    }
-                }}><img style={{ width: 20, zIndex: 2 }} src={alarm.img} alt="like" /></Button>
+                    if (alarm.id === 'alarmunlike') Events(Const.events.productAlarmLike.type, props)
+                    else Events(Const.events.productAlarmUnlike.type, props)
+                }}>
+                    <Image src={alarm.img} fluid />
+                </Button>
             </div>
-            <Card.Img className="SeachCard-topImg" variant="top" src={props.productImageSrc} />
-            <Card.Body className="SeachCard-body">
-                <Card.Title className="SeachCard-title">{props.productTitle}</Card.Title>
-                <div className="SeachCard-priceBody">
-                    {offerArr}
-                </div>
+            <Card.Img className="searchCard-Img" variant="top" src={props.productImageSrc} />
+            <Card.Body className="searchCard-body">
+                <Card.Title className="searchCard-body-title">{props.productTitle}</Card.Title>
+                <div className="searchCard-body-priceBody">{offerArr}</div>
             </Card.Body>
         </Card>
     )
