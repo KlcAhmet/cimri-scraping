cca9b0c829a3b5223b87dfc5d60d787ffafc06ec
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-10 01:27:07 +0300

    create Page404.js

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,7 +1,15 @@
+import { Row, Col, Image } from 'react-bootstrap';
+import { page404gif } from '../map/ComponentMap'
+
 const Page404 = props => {
 
     return (
-        <p>404 page</p>
+        <Row  >
+            <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} xs={11} sm={11} md={11} lg={11}>
+                <h6 style={{ textAlign: 'center', fontSize: 60, color: 'red', margin: 50 }}>404 sayfa bulunamadı</h6>
+                <Image src={page404gif} fluid />
+            </Col>
+        </Row>
     )
 }
 
