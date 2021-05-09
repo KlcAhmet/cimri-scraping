bccf65e9a64e198cf8646227eb25430cc85644bb
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-10 01:49:45 +0300

    ProductNotFound.js içerik yazıldı

--- src/pages/ProductNotFound.js
+++ src/pages/ProductNotFound.js
@@ -1,5 +1,16 @@
+import { Icon, IconSize, Intent } from "@blueprintjs/core"
+import { IconNames } from "@blueprintjs/icons"
+import { Row, Col } from 'react-bootstrap';
+
 const ProductNotFound = () => {
-    return <p>Ürün Bulunamadı</p>
+    return (
+        <Row  >
+            <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} xs={11} sm={11} md={11} lg={11}>
+                <h6 style={{ textAlign: 'center', fontSize: 40, color: 'red', margin: 50 }}>Ürün bulanamadı.</h6>
+                <Icon icon={IconNames.ZOOM_OUT} iconSize={100} intent={Intent.PRIMARY} />
+            </Col>
+        </Row>
+    )
 }
 
 export default ProductNotFound
