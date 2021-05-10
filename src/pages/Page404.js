fd4bf8ba5683580b7ecbca81da7582c0dd9df6e0
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-05-10 03:06:27 +0300

    final initalize

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -4,8 +4,8 @@ import { page404gif } from '../map/ComponentMap'
 const Page404 = props => {
 
     return (
-        <Row  >
-            <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} xs={11} sm={11} md={11} lg={11}>
+        <Row style={{ margin: 0 }} >
+            <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} xs={12} sm={12} md={12} lg={12}>
                 <h6 style={{ textAlign: 'center', fontSize: 60, color: 'red', margin: 50 }}>404 sayfa bulunamadı</h6>
                 <Image src={page404gif} fluid />
             </Col>
--- src/pages/ProductNotFound.js
+++ src/pages/ProductNotFound.js
@@ -4,7 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 
 const ProductNotFound = () => {
     return (
-        <Row  >
+        <Row style={{ margin: 0 }} >
             <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} xs={11} sm={11} md={11} lg={11}>
                 <h6 style={{ textAlign: 'center', fontSize: 40, color: 'red', margin: 50 }}>Ürün bulanamadı.</h6>
                 <Icon icon={IconNames.ZOOM_OUT} iconSize={100} intent={Intent.PRIMARY} />
